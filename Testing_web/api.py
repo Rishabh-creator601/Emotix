07ec2de7f9089e2287dbb53e80f90969c5c79014
Hariomji4 <newera82882@gmail.com>
2026-07-18 20:00:29 +0530

    Update CORS configuration to allow Netlify frontend

--- Testing_web/api.py
+++ Testing_web/api.py
@@ -25,6 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://curious-pie-9d1f7d.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
